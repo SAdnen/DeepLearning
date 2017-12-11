4fbf1e3a6f2ef1ed2ea3bc8925e378cb3b99ccd0
SAdnen <saadaoui.adnen@gmail.com>
2017-12-11 20:54:39 +0100

    Second Commit

--- mnist.py
+++ mnist.py
@@ -14,9 +14,44 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 # --------- Import Mnist dataset
+
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 plt.rcParams['figure.figsize'] = (5.0, 5.0)
 sampleimage = mnist.test.images[2].reshape([28, 28])
 plt.imshow(sampleimage, cmap="gray")
-
 x = sampleimage.reshape([1, 28, 28, 1])
+
+# %% Define Placeholders and Variables
+
+X = tf.placeholder(tf.float32, shape=(1, 28, 28, 1), name="Input_Image")
+W1 = tf.Variable(tf.truncated_normal((5, 5, 1, 1), stddev=0.1))
+b1 = tf.Variable(tf.zeros(32, 1))
+
+# %% Define first confolutional layer
+
+Conv1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], paddin='SAME') + b1
+Conv1out = tf.nn.relu(Conv1)
+init = tf.global_variables_initializer()
+with tf.Session as sess:
+    sess.run(init)
+    result = sess.run(Conv1out, feed_dict={X: x})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
